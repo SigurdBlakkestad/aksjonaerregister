71a2cee4f8b879cfeef98afdeb7e55428590ac68
Sigurd Blakkestad <sigurdbla@gmail.com>
2026-03-24 10:47:26 +0100

    chore: deleting deprecated code

--- src/main/kotlin/no/sigurd/aksjonaerregister/Message.kt
+++
@@ -1,9 +0,0 @@
-//package no.sigurd.aksjonaerregister
-//
-//import jakarta.persistence.Entity
-//import jakarta.persistence.Id
-//import jakarta.persistence.Table
-//
-//@Entity
-//@Table(name = "MESSAGES")
-//data class Message(val text: String, @Id val id: String? = null)
--- src/main/kotlin/no/sigurd/aksjonaerregister/MessageRepository.kt
+++
@@ -1,5 +0,0 @@
-//package no.sigurd.aksjonaerregister
-//
-//import org.springframework.data.repository.CrudRepository
-//
-//interface MessageRepository : CrudRepository<Message, String>
