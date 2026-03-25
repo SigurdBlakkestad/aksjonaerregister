4c1354bbb960b9b814c7b5ea666c331bb33aaf26
Sigurd Blakkestad <sigurdbla@gmail.com>
2026-03-25 13:55:30 +0100

    feat: updating with real data

--- src/main/kotlin/no/sigurd/aksjonaerregister/controller/AksjonaerController.kt
+++ src/main/kotlin/no/sigurd/aksjonaerregister/controller/AksjonaerController.kt
@@ -1,7 +1,11 @@
 package no.sigurd.aksjonaerregister.controller
 
-import no.sigurd.aksjonaerregister.model.Aksjonaer
+import no.sigurd.aksjonaerregister.model.dto.TransaksjonsRequestDTO
+import no.sigurd.aksjonaerregister.model.entity.Aksjonaer
+import no.sigurd.aksjonaerregister.model.entity.Transaksjon
+import no.sigurd.aksjonaerregister.model.enums.TransaksjonType
 import no.sigurd.aksjonaerregister.service.AksjonaerService
+import no.sigurd.aksjonaerregister.service.TransaksjonService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -9,10 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/aksjonaerer")
-class AksjonaerController(private val aksjonaerService: AksjonaerService) {
+class AksjonaerController(private val aksjonaerService: AksjonaerService,
+                          private val transaksjonService: TransaksjonService
+) {
 
     @PostMapping
     fun nyAksjonaer(@RequestBody aksjonaer: Aksjonaer) : ResponseEntity<Aksjonaer> {
@@ -27,4 +36,35 @@ class AksjonaerController(private val aksjonaerService: AksjonaerService) {
     }
 
     @PostMapping("/{fnr}/transaksjoner")
+        fun registrerTransaksjon(@PathVariable fnr: String, @RequestBody request: TransaksjonsRequestDTO): ResponseEntity<Transaksjon>{
+            val nyTransaksjon = transaksjonService.registrerTransakjson(fnr, request)
+            return ResponseEntity.ok(nyTransaksjon)
+        }
+//            val opprettet = transaksjonService.registrerTransaksjonForAksjonaer(fnr, request)
+//            return ResponseEntity
+//                .created(URI("/api/aksjonaerer/$fnr/transaksjoner/${opprettet.id}"))
+//                .body(opprettet)
+
+    @GetMapping("/{fnr}/transaksjoner")
+    fun hentTransaksjoner(@PathVariable fnr: String): ResponseEntity<List<Transaksjon>> {
+        return ResponseEntity.ok(transaksjonService.hentTransaksjonerForAksjonaer(fnr))
+    }
+
+    private fun dummyTransaksjon(fnr: String): Transaksjon {
+        val dummyAksjonaer = Aksjonaer(
+            id = UUID.randomUUID(),
+            navn = "Ola Nordmann",
+            foedselsnummer = fnr
+        )
+
+        return Transaksjon(
+            id = UUID.randomUUID(),
+            aksjonaer = dummyAksjonaer,
+            aksjenavn = "Equinor",
+            transaksjonType = TransaksjonType.KJOEP,
+            antall = 100,
+            prisPerAksje = BigDecimal("250.50"),
+            dato = LocalDate.now()
+        )
+    }
 }
