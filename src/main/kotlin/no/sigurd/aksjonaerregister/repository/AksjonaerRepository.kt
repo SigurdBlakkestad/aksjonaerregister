c281116fd79dad8ceb004c1b4d7b3134310fe44a
Sigurd Blakkestad <sigurdbla@gmail.com>
2026-03-25 13:55:46 +0100

    feat: getting repositories up do date

--- src/main/kotlin/no/sigurd/aksjonaerregister/repository/AksjonaerRepository.kt
+++ src/main/kotlin/no/sigurd/aksjonaerregister/repository/AksjonaerRepository.kt
@@ -1,9 +1,10 @@
 package no.sigurd.aksjonaerregister.repository
 
-import no.sigurd.aksjonaerregister.model.Aksjonaer
+import no.sigurd.aksjonaerregister.model.entity.Aksjonaer
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 import java.util.UUID
 
 interface AksjonaerRepository : JpaRepository<Aksjonaer, UUID> {
-    fun findByFoedselsnummer(foedselsnummer: String): Aksjonaer?
+    fun findByFoedselsnummer(foedselsnummer: String): Optional<Aksjonaer>
 }
--- src/main/kotlin/no/sigurd/aksjonaerregister/repository/TransaksjonRepository.kt
+++ src/main/kotlin/no/sigurd/aksjonaerregister/repository/TransaksjonRepository.kt
@@ -1,4 +1,15 @@
 package no.sigurd.aksjonaerregister.repository
 
-interface TransaksjonRepository {
+import no.sigurd.aksjonaerregister.model.entity.Aksjonaer
+import no.sigurd.aksjonaerregister.model.entity.Transaksjon
+import no.sigurd.aksjonaerregister.model.enums.TransaksjonType
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.List
+import java.util.UUID
+
+@Repository
+interface TransaksjonRepository : JpaRepository<Transaksjon, UUID> {
+    fun findByAksjonerFoedselsnuummer(foedselsnummer: String): List<Transaksjon>
+    fun findByAksjonaerFoedselsnummer(aksjonaerFoedselsnummer: String): MutableList<Transaksjon>
 }
